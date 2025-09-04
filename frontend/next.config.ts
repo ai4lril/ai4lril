8064d473c5f549ba6ab38e377d725e14e18f517f
Alvyn Abranches <alvynabranches@gmail.com>
2025-09-04 23:42:15 +0530

    Prod build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // basePath: "/voice-data-collection",
-  // assetPrefix: "/voice-data-collection/",
+  basePath: "/voice-data-collection",
+  assetPrefix: "/voice-data-collection/",
   trailingSlash: true,
 };
 
